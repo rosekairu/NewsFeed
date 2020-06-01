61bccddd9f25af1ee2503f1aac1cea0cb813a2f2
Rose Kairu <rosekairu@gmail.com>
2020-06-01 23:51:51 +0300

    modified congif file

--- config.py
+++ config.py
@@ -2,19 +2,10 @@ import os
 
 class Config:
     
-    #NEWS_API_BASE_URL="https://newsapi.org/v2/{}?country=us&apiKey={}"
-    
-    #NEWS_BASE_URL="https://newsapi.org/v2/{}?language=en&apiKey={}"#sources api
-    
-    #NEWS_ARTICLE_API="https://newsapi.org/v2/top-headlines?sources={}&apiKey={}"
-    
-    SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?category={}&apiKey={}'
-    
-    ARTICLE_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
-
-    NEWS_API_KEY=os.environ.get("NEWS_API_KEY")
-    
-    SECRET_KEY=os.environ.get("SECRET_KEY")
+        NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+        ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+        NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+        # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
